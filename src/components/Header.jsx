4fd53f46f6660a34576b643cffcedfd7c1c4e2fa
Khalid Mansoor <97522481+MansoorEhmad@users.noreply.github.com>
2024-04-01 15:04:13 +0500

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
             <div className="container-fluid">
                 <Link className="navbar-brand" to={"/"}><img src="./logo.svg" style={{width: "25px"}}/></Link>
                 <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon text-white"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
